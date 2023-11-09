7c92b20d14a3e39f230a2f27d9f0c33230e50f1e
Vladimir Dementyev <palkan@hey.com>
2023-11-09 12:24:47 -0800

    fix: include app/ folder into gem package

--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-RailsIntestViews::Engine.routes.draw do
-  get "/template/:id", to: "templates#show", as: :rails_intest_template
-end
--- lib/rails-intest-views/version.rb
+++ lib/rails-intest-views/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module RailsIntestViews # :nodoc:
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
